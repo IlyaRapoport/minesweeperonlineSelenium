6d4ad07d05bf1798fa8f3e2e31463917d54bfcfa
ilya.rapoports <ilja.rapoport@gmail.com>
2019-05-29 11:17:26 +0300

    Ilya

--- src/main/java/MineSweeper.java
+++ src/main/java/MineSweeper.java
@@ -13,15 +13,16 @@ import java.util.List;
 
 public class MineSweeper {
     public static String libWithDriversLocation = System.getProperty("user.dir") + "\\src\\main\\resources\\";
-public static int x;
-public static int y;
-public static int[][] field;
+    public static int x;
+    public static int y;
+    public static int[][] field;
 
-    public static  WebDriver driver;
+    public static WebDriver driver;
+
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
-          driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
 
@@ -31,51 +32,47 @@ public static int[][] field;
         String dimensions = driver.findElement(By.cssSelector("#game div:last-child")).getAttribute("id");
 
 
-
-
-       String[]  splitedLasteelement= dimensions.split("_");
+        String[] splitedLasteelement = dimensions.split("_");
 
         y = Integer.parseInt(splitedLasteelement[0]);
-         x = Integer.parseInt(splitedLasteelement[1])-1;
+        x = Integer.parseInt(splitedLasteelement[1]) - 1;
 
 
         field = new int[x][y];
 
 
-
-
-findElements();
-click();
-Thread.sleep(1000);
+        findElements();
+        click();
+        Thread.sleep(1000);
         driver.quit();
 
     }
 
-public static void findElements(){
+    public static void findElements() {
 
-    for (int i = 1; i <=y ; i++) {
-        for (int j = 1; j <=x ; j++) {
-            String value = driver.findElement(By.id(i+"_"+j)).getAttribute("class");
+        for (int i = 1; i <= y; i++) {
+            for (int j = 1; j <= x; j++) {
+                String value = driver.findElement(By.id(i + "_" + j)).getAttribute("class");
 
-            if (value.equals("square blank")) field[j-1][i-1]=-1;
-            if (value.equals("square open0")) field[j-1][i-1]=0;
-            if (value.equals("square open1")) field[j-1][i-1]=1;
-            if (value.equals("square open2")) field[j-1][i-1]=2;
-            if (value.equals("square open3")) field[j-1][i-1]=3;
-            if (value.equals("square open4")) field[j-1][i-1]=4;
-            if (value.equals("square open5")) field[j-1][i-1]=5;
-            if (value.equals("square open6")) field[j-1][i-1]=6;
-            if (value.equals("square open7")) field[j-1][i-1]=7;
-            if (value.equals("square open8")) field[j-1][i-1]=8;
+                if (value.equals("square blank")) field[j - 1][i - 1] = -1;
+                if (value.equals("square open0")) field[j - 1][i - 1] = 0;
+                if (value.equals("square open1")) field[j - 1][i - 1] = 1;
+                if (value.equals("square open2")) field[j - 1][i - 1] = 2;
+                if (value.equals("square open3")) field[j - 1][i - 1] = 3;
+                if (value.equals("square open4")) field[j - 1][i - 1] = 4;
+                if (value.equals("square open5")) field[j - 1][i - 1] = 5;
+                if (value.equals("square open6")) field[j - 1][i - 1] = 6;
+                if (value.equals("square open7")) field[j - 1][i - 1] = 7;
+                if (value.equals("square open8")) field[j - 1][i - 1] = 8;
 
+
+            }
 
         }
 
     }
 
-}
-
-    public static void click(){
+    public static void click() {
 //        Pair<Integer, Integer>[] coord;
 //        coord[0].getKey();
 //        coord[0].getValue();
